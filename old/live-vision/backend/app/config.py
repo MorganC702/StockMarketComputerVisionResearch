baa6a95631971016582fe219ddcf86f751ab4f70
Morgan Cooper <morgan.cooper702@gmail.com>
2025-09-26 15:24:42 -0700

    .

--- old/live-vision/backend/app/config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-# Load variables from .env
-load_dotenv()
-
-POLYGON_API_KEY = os.getenv("POLYGON_API_KEY")
-POLYGON_SUBSCRIPTIONS = os.getenv("POLYGON_SUBSCRIPTIONS", "AM.SPY").split(",")
